acff87954f25262acea5faae1c9cd7e38433da12
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-07-03 12:15:28 +0200

    layout done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen items-center justify-between">
+      {/*   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
-      </div>
+      </div> */}
+      <nav className="flex bg-secBlue w-24 h-screen m-8 rounded-[20px]"></nav>
     </main>
   );
 }
