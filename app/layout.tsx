fcca9956211140448a9715102597b44f1648c88f
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-07-04 13:46:18 +0200

    nav is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 export const metadata = {
   title: "entertainment app",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-between">
-      {/*   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex flex-col min-h-screen w-full mt-16 mx-9">
+      <section className=" flex gap-6 w-full">
+        <Image
+          src="./images/icon-search.svg"
+          alt="search"
+          width="32"
+          height="32"
+        />
+        <input
+          className="bg-mainBlue w-full text-HeadingM opacity-50 text-pureWhite"
+          type="text"
+          placeholder="Search for movies or TV series"
+        />
+      </section>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
-      </div> */}
-      <nav className="flex bg-secBlue w-24 h-screen m-8 rounded-[20px]"></nav>
+      </div>
     </main>
   );
 }
